305e9c5263bb7a0996d5cdfbac6d3d7487ed9907
RAHUL <rahulborase946@gmail.com>
2024-07-15 17:55:32 +0530

    java program without a main method

---
+++ src/ex_07072024/lab004.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+//we can execute a java program without a main method by using a static block
+
+public class lab004 {
+    static{
+        System.out.println("class without main method");
+        System.out.println(status 0);
+    }
+    //we execute java program without main method (work until java version 1.6)
+    //jav 7 and new version don't allow this because JVM checks the presence main method
+}

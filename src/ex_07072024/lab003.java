7fa0bbabe0388c3fb0243163e2d603248952ae1f
RAHUL <rahulborase946@gmail.com>
2024-07-17 14:37:55 +0530

    Process finished with exit code

--- src/ex_07072024/lab003.java
+++ src/ex_07072024/lab003.java
@@ -5,6 +5,8 @@ public class lab003 {
         System.out.println(2+2);
         System.out.println(2888+25558);
         System.out.println(26/0); //devided by zero no value program run with exception
+        // Process finished with exit code 1.it means your code is not fine
+
          // use for single comment
         /*use for multiple comments for document purpose
          *

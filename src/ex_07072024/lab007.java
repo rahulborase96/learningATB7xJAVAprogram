8974ec4f4ebeead2c161e6ad42833d1e52e8e44d
RAHUL <rahulborase946@gmail.com>
2024-07-17 15:19:07 +0530

    Shortcut keys

--- src/ex_07072024/lab007.java
+++ src/ex_07072024/lab007.java
@@ -2,10 +2,10 @@ package ex_07072024;
 
 public class lab007 {
     public static void main(String[] args) {
-        System.out.println("shortcut keys");
-        System.out.println("for comment (ctrl + /)");
-        System.out.println("for copy paste of sout (ctrl + d)");
-        System.out.println("for refactor(Rename) (ctrl + Alt+ Shift +t)");
+        System.out.println("Shortcut Keys");
+        System.out.println("1 for comment (ctrl + /)");
+        System.out.println("2 for copy paste of sout (ctrl + d)");
+        System.out.println("3 for refactor(Rename) (ctrl + Alt+ Shift +t)");
 //        System.out.println();
 //        System.out.println();
 //        System.out.println();

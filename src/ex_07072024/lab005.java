d851d032f302a69967c9df46d6ee053f3cfdd05a
RAHUL <rahulborase946@gmail.com>
2024-07-17 14:32:24 +0530

    java program path

---
+++ src/ex_07072024/lab005.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class lab005 {
+    public static void main(String[] args) {
+
+        //Package->Class ->Main method->Statement (instructions)
+
+        System.out.println("good morning all of you");
+
+            }
+}

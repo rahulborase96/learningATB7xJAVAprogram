55500423e9665741ee6d6b9f142071d38b872e07
RAHUL <rahulborase946@gmail.com>
2024-07-17 14:31:25 +0530

    java program without a main method

--- src/ex_07072024/lab004.java
+++ src/ex_07072024/lab004.java
@@ -4,8 +4,8 @@ package ex_07072024;
 public class lab004 {
     static{
         System.out.println("class without main method");
-        System.out.println(status 0);
+        System.out.println(0);
     }
     //we execute java program without main method (work until java version 1.6)
-    //java 7 and new version don't allow this because JVM checks the presence main method
+    //jav 7 and new version don't allow this because JVM checks the presence main method
 }

5c7ea9e102a6deda62095ccef99a0c0b8bfd9b21
RAHUL <rahulborase946@gmail.com>
2024-07-17 14:24:41 +0530

    program run with exception

--- src/ex_07072024/lab003.java
+++ src/ex_07072024/lab003.java
@@ -4,10 +4,10 @@ public class lab003 {
     public static void main(String[] args) {
         System.out.println(2+2);
         System.out.println(2888+25558);
-        System.out.println(26/0); //devided by zero no value
-         // use for cooment
-        /*use for multiple comments
-         * program run with exception
+        System.out.println(26/0); //devided by zero no value program run with exception
+         // use for single comment
+        /*use for multiple comments for document purpose
+         *
          *
          *
          *

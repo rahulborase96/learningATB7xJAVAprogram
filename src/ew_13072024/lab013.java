7fcc5bb6f1f64a72d3b1d0bf10e09c0bac5d7f97
RAHUL <rahulborase946@gmail.com>
2024-07-18 16:30:24 +0530

    practice of data type

---
+++ src/ew_13072024/lab013.java
@@ -0,0 +1,15 @@
+package ew_13072024;
+
+public class lab013 {
+    public static void main(String[] args) {
+
+        byte a= 10;
+        System.out.println(a);
+
+                //10- decimal no. its binary no. is= 00001010
+        //machine language (0,1) = 00001010 -> 8 bits = 1 Byte
+            long no = 3500000000; //only stored 64bits = 8 Byte
+        System.out.println(no);
+        // iys shown "integer number too large"
+    }
+}
---
+++ src/ew_13072024/lab015.java
@@ -0,0 +1,4 @@
+package ew_13072024;
+
+public class lab015 {
+}

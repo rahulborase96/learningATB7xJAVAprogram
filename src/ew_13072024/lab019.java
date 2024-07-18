aba456a8337a863cb3aac2b8f1e982807d729678
RAHUL <rahulborase946@gmail.com>
2024-07-18 17:34:56 +0530

    formatting

--- src/ew_13072024/lab019.java
+++ src/ew_13072024/lab019.java
@@ -2,8 +2,14 @@ package ew_13072024;
 
 public class lab019 {
     public static void main(String[] args) {
+      //formating
+        int age =28;
 
-        System.out.println();
-
-    }
+        System.out.println("My  age is "  + age);//here is concatenation happen
+//        %d ->  any integer - int,byte,short and long
+//        %s ->  String
+//        %c -> char
+//        %f -> float
+        System.out.printf("My  age is -> %d", age); //Use printf for formatting
+           }
 }

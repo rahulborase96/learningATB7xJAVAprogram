495536c5238130e22e8f7de945250aa2a4f74751
RAHUL <rahulborase946@gmail.com>
2024-07-18 15:41:28 +0530

    non-primitive data type (String)

---
+++ src/ew_13072024/lab012.java
@@ -0,0 +1,14 @@
+package ew_13072024;
+
+public class lab012 {
+    public static void main(String[] args) {
+        //non-primitive data type
+        //String name = 'Rahul', its is bunch of character or numbers
+        String name = "Rahul"; //its written in (" ")
+        String surname ="borase";
+        System.out.println(name+surname);
+
+
+    }
+
+}

f3e8d6a9bbb03c77abf3418137346f5e7a630672
RAHUL <rahulborase946@gmail.com>
2024-07-19 16:41:52 +0530

    practice files on operators

---
+++ src/ex_14072024/lab036.java
@@ -0,0 +1,22 @@
+package ex_14072024;
+
+public class lab036 {
+    public static void main(String[] args) {
+
+        //binary operator
+
+        int a =20;
+        int b =26;
+
+        int sum = a+b;
+
+                // two operand
+           // a, b -> operand ,   + operator
+        System.out.println(sum);
+
+
+
+
+
+    }
+}
---
+++ src/ex_14072024/lab037.java
@@ -0,0 +1,20 @@
+package ex_14072024;
+
+public class lab037 {
+    public static void main(String[] args) {
+        // =  And  ==
+
+        int a= 10;   //= to assign the value
+        int b =22;
+
+boolean c = a==b;   //== ti compare the value
+boolean d = a<b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+
+    }
+}

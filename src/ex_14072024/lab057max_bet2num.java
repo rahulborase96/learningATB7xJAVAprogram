fa6bbcf71ed08160bffbac8ae096d9a6747646b0
RAHUL <rahulborase946@gmail.com>
2024-07-21 21:31:39 +0530

    practice files

---
+++ src/ex_14072024/lab057max_bet2num.java
@@ -0,0 +1,21 @@
+package ex_14072024;
+
+public class lab057max_bet2num {
+    public static void main(String[] args) {
+         int a =20;
+         int b =26;
+
+         int max = a > b ? a : b;
+
+
+        System.out.println(max);
+      int min = a<b ? a:b;
+        System.out.println(min);
+
+
+    }
+
+
+
+
+}
---
+++ src/ex_14072024/lab058math_class.java
@@ -0,0 +1,25 @@
+package ex_14072024;
+import java.lang.Math;
+
+public class lab058math_class {
+    public static void main(String[] args) {
+
+        double sq = Math.sqrt(5);
+        double sq2 = Math.sqrt(25);
+        double sq3 = Math.sqrt(55);
+        double sq4 = Math.sqrt(555);
+        System.out.println(sq);
+        System.out.println(sq2);
+        System.out.println(sq3);
+        System.out.println(sq4);
+
+        int result =Math.max(45,85);
+        System.out.println(result);
+
+
+    }
+
+
+
+
+}
